e38166b57d312fd9f5b531929452c9b747b77681
Anthony Garitachea <lgaritachea45204@gmail.com>
2019-09-11 11:27:17 -0700

    added history component

---
+++ src/History.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class History extends React.Component {
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return (
+            <div>
+               <h3>San Diego</h3>
+               <p>09/10/2019</p>
+               <p>timestamp</p>
+            </div>
+        )
+    }
+}
+
+export default History
